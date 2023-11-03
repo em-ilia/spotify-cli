2f184e4a7d041947cbc79b6999aefac1d0e504be
Emilia <contact@emiliaallison.com>
2023-11-03 14:11:21 -0400

    Feature json_export

--- src/commands/debug.rs
+++ src/commands/debug.rs
@@ -1,5 +1,8 @@
 use std::path::PathBuf;
 
+#[cfg(feature = "json_export")]
+use serde_json;
+
 use crate::{commands::auth, spotify, util};
 
 pub fn dump_playlist(path: PathBuf, playlist: &str) {
@@ -13,7 +16,31 @@ pub fn dump_playlist(path: PathBuf, playlist: &str) {
 
     let res = spotify::playlist::get_playlist_items(playlist, &token);
 
-    println!("{:?}", res);
+    // We will use serde_json to export as JSON if the "json_export" feature is enabled,
+    // otherwise, we'll just dump the derived Debug of everything.
+    if cfg!(feature = "json_export") {
+        #[cfg(feature = "json_export")]
+        {
+        match res {
+            Ok(data) => {
+                let table = serde_json::to_string(&data);
+                match table {
+                    Ok(table) => {
+                        println!("{}", table.to_string());
+                    }
+                    Err(e) => {
+                        println!("Failed to serialize data:\n{:?}", e);
+                    }
+                }
+            }
+            Err(e) => {
+                println!("Failed to get playlist items:\n{:?}", e);
+            }
+        }
+        }
+    } else {
+        println!("{:?}", res);
+    }
 }
 
 pub fn clear_playlist(path: PathBuf, playlist: &str) {
